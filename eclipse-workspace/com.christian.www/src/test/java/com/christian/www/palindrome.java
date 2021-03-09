c9fee555f36383ab21b19d268b64d0e0bc25756c
bafang1981 <bafang1981@users.noreply.github.com>
2021-03-08 18:38:11 -0600

    # WARNING: head commit changed in the meantime
    ceci est un test237

---
+++ eclipse-workspace/com.christian.www/src/test/java/com/christian/www/palindrome.java
@@ -0,0 +1,52 @@
+package com.christian.www;
+
+public class palindrome {
+
+	
+		
+	public class lab4 {
+
+		private String mane = "BMW";
+		private double price = 5000;
+			private int quantity = 4;
+		
+		public lab4(String name, double price, int quantity) {
+
+			
+		
+			
+			
+		}
+
+		public double getPrice() {
+			return price;
+		}
+
+		public void setPrice(double price) {
+			this.price = price;
+			
+			System.out.println("this is the best price for this car");
+		}
+
+		public int getQuantity() {
+			return quantity;
+			
+			
+		}
+
+		public void setQuantity(int quantity) {
+			this.quantity = quantity;
+			System.out.println("this is the number I need");
+		}
+
+		public String getMane() {
+			return mane;
+		}
+
+		public void setMane(String mane) {
+			this.mane = mane;
+			System.out.println("BMW car is the best");
+		}
+
+	}
+}
